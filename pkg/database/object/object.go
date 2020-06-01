bc4e3eff9922b2062ed0d74e2d20fcf478630743
cbrom abody <kb.cbrom@gmail.com>
2020-06-01 21:00:11 +0300

    database handler created - storage interface created - postgresql driver of storage interface implemented - current handler holder created - New db handler object created flag processer added  - a copy paste from previous project

---
+++ pkg/database/object/object.go
@@ -0,0 +1,44 @@
+package object
+
+import "time"
+
+//Interface defines model structs
+type Interface interface {
+	GetNameSpace() string
+	GetID() string
+	SetID(string)
+	SetCreatedAt(t time.Time)
+	SetUpdatedAt(t time.Time)
+	SetArchivedAt(t *time.Time)
+}
+
+//Interfaces
+type Interfaces interface {
+	GetNameSpace() string
+}
+
+//Pagination defines list page pagination and sorting query
+type Pagination interface {
+	GetPage() uint16
+	GetLimit() uint16
+	GetHasNext() bool
+	GetHasPrev() bool
+	SetHasNext(bool)
+	SetHasPrev(bool)
+	GetPages() []string
+	SetPages([]string)
+	GetSortField() string
+	GetSortOrder() string
+}
+
+//AdvancedSearch defines query for like and in query
+type AdvancedSearch interface {
+	GetSearchField() string
+	SetSearchField(string)
+	GetSearchQuery() string
+	SetSearchQuery(string)
+	GetSearchInField() string
+	SetSearchInField(string)
+	GetSearchInQuery() []interface{}
+	SetSearchInQuery([]interface{})
+}
