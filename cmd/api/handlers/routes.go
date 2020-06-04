c01438b4d724fad6dca7d77586d280293ab4ddad
cbrom abody <kb.cbrom@gmail.com>
2020-06-04 22:24:02 +0300

    ping handler and ping added

--- cmd/api/handlers/routes.go
+++ cmd/api/handlers/routes.go
@@ -6,6 +6,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	authenticator *auth.Authenticator
+)
+
 func pingHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
@@ -14,6 +18,7 @@ func pingHandler(c *gin.Context) {
 
 // ApplyRoutes applies router to gin engine
 func ApplyRoutes(r *gin.Engine, authenticator *auth.Authenticator) {
+	authenticator = authenticator
 	apiV1 := r.Group("/v1")
 	{
 		apiV1.GET("/ping", pingHandler)
