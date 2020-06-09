74fc84985c7e0002c05fa9b00c9ddd3f34264827
cbrom abody <kb.cbrom@gmail.com>
2020-06-09 13:41:16 +0300

    comments on exporeted types and functions - error values ported here

--- cmd/api/handlers/user.handler.go
+++ cmd/api/handlers/user.handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"errors"
 	"go-lms-of-pupilfirst/cmd/api/handlers/reqres"
 	"go-lms-of-pupilfirst/pkg/auth"
 	"log"
@@ -8,8 +9,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	errAuthenticationFailure = errors.New("Authentication failed")
+	errorNotFound            = errors.New("Entity not found")
+	errForbidden             = errors.New("Attempted action is not allowed")
+	errUnableToCreateUser    = errors.New("Unable to create User")
+	errUnableToFetchUser     = errors.New("Unable to fetch user")
+	errUnableToFetchUserList = errors.New("Unable to fetch user list")
+	errUnableToUpdateUser    = errors.New("Unable to update user")
+	errUnableToDeleteUser    = errors.New("Unable to delete user")
+
+	// ErrResetExpired occurs when the reset hash exceeds the expiration
+	ErrResetExpired = errors.New("Reset expired")
+)
+
+// UserController is an anonymous struct for user controller
 type UserController struct{}
 
+// SignUp registers user
 func (ctrl *UserController) SignUp(ctx *gin.Context) {
 	// get values
 	// build into struct
