6fdb8d8960d749ab9c19f7f101c34ccbc5fb3577
cbrom abody <kb.cbrom@gmail.com>
2020-06-11 21:59:26 +0300

    authentication middleware created and Basic function implemented to access Autherization from header

---
+++ pkg/middlewares/auth.go
@@ -0,0 +1,38 @@
+package middlewares
+
+import (
+	"context"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+
+	"go-lms-of-pupilfirst/pkg/auth"
+)
+
+// JWTAuthMiddleware gets token from header and sets claims and auth key in the context
+func JWTAuthMiddleware(authenticator *auth.Authenticator) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		tokenString, err := ctx.Cookie("token")
+
+		// failed to read cookie
+		if err != nil {
+			// try Http Header
+			authorization := ctx.Request.Header.Get("Authorization")
+			if authorization != "" {
+				sp := strings.Split(authorization, "Bearer ")
+				// invalid token
+				if len(sp) >= 1 {
+					tokenString = sp[1]
+				}
+			}
+
+		}
+
+		claims, _ := authenticator.ParseClaims(tokenString)
+		ctx.Request = ctx.Request.WithContext(context.WithValue(ctx.Request.Context(), tokenString, claims))
+
+		// ctx.Set("claims", claims)
+		// ctx.Set("auth_key", auth.Key)
+		ctx.Next()
+	}
+}
