de466c49e608ac868cf04fc712252bdf4d1153a3
cbrom abody <kb.cbrom@gmail.com>
2020-06-13 22:37:57 +0300

    certificate crud functions implemented

--- cmd/models/certificate.model.go
+++ cmd/models/certificate.model.go
@@ -4,6 +4,10 @@ import (
 	"go-lms-of-pupilfirst/pkg/utils"
 )
 
+var (
+	certificateTableName = "certificates"
+)
+
 // Certificate defines a model for student certificates in a model
 type Certificate struct {
 	utils.Base
@@ -16,4 +20,61 @@ type Certificate struct {
 	FontSize      int
 	Message       string
 	Active        bool
+}
+
+// TableName gorm standard table name
+func (u *Certificate) TableName() string {
+	return certificateTableName
+}
+
+// CertificateList defines array of certificate objects
+type CertificateList []*Certificate
+
+// TableName gorm standard table name
+func (u *CertificateList) TableName() string {
+	return certificateTableName
+}
+
+/**
+CRUD functions
+*/
+
+// Create creates a new certificate record
+func (u *Certificate) Create() error {
+	possible := handler.NewRecord(u)
+	if possible {
+		if err := handler.Create(u).Error; err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// FetchByID fetches Certificate by id
+func (u *Certificate) FetchByID() error {
+	err := handler.First(u).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// FetchAll fetchs all Certificates
+func (u *Certificate) FetchAll(ul *CertificateList) error {
+	err := handler.Find(ul).Error
+	return err
+}
+
+// UpdateOne updates a given certificate
+func (u *Certificate) UpdateOne() error {
+	err := handler.Save(u).Error
+	return err
+}
+
+// Delete deletes certificate by id
+func (u *Certificate) Delete() error {
+	err := handler.Delete(u).Error
+	return err
 }
