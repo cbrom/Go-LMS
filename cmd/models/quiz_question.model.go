05ca4ff2e3892de0c003ba6a70250a95d421c23a
cbrom abody <kb.cbrom@gmail.com>
2020-06-13 16:24:32 +0300

    type (Quize)

--- cmd/models/quiz_question.model.go
+++ cmd/models/quiz_question.model.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"go-lms-of-pupilfirst/pkg/utils"
+)
+
+// QuizQuestion defines a model for questions in a quiz
+type QuizQuestion struct {
+	utils.Base
+	QuizID          string `sql:"type:uuid;" validate:"omitempty,uuid,required"`
+	Question        string
+	Description     string `gorm:"type:varchar(100)"`
+	CorrectAnswerID string `sql:"type:uuid;" validate:"omitempty,uuid,required"`
+}
