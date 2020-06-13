c9d9f9de4c111f698b55ff07a4ea9b3e152a3b75
cbrom abody <kb.cbrom@gmail.com>
2020-06-13 16:10:59 +0300

    certificate changed to be a basic certificate and not a user issued certificate

--- cmd/models/certificate.model.go
+++ cmd/models/certificate.model.go
@@ -7,9 +7,13 @@ import (
 // Certificate defines a model for student certificates in a model
 type Certificate struct {
 	utils.Base
-	CourseID string `sql:"type:uuid;" validate:"omitempty,uuid,required"`
-	IssuerID string `sql:"type:uuid;" validate:"omitempty,uuid,required"`
-	UserID   string `sql:"type:uuid;" validate:"omitempty,uuid,required"`
-	Message  string
-	Active   bool
+	CourseID      string `sql:"type:uuid;" validate:"omitempty,uuid,required"`
+	IssuerID      string `sql:"type:uuid;" validate:"omitempty,uuid,required"`
+	QRCorner      string
+	QRScale       int
+	Margin        int
+	NameOffsetTop int
+	FontSize      int
+	Message       string
+	Active        bool
 }
