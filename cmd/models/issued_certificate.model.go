819c56e7278b3b44c98aa1f65cd5c40c7e19598f
cbrom abody <kb.cbrom@gmail.com>
2020-06-13 16:15:47 +0300

    basic model for issued certificates implemented

---
+++ cmd/models/issued_certificate.model.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"go-lms-of-pupilfirst/pkg/utils"
+)
+
+// IssuedCertificate defines a model for user certificates for a course
+type IssuedCertificate struct {
+	utils.Base
+	CertificateID string `sql:"type:uuid;" validate:"omitempty,uuid,required"`
+	UserID        string `sql:"type:uuid;" validate:"omitempty,uuid,required"`
+	SerialNumber  string `sql:"type:citext;"`
+}
