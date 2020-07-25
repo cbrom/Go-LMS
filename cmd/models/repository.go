9872020c2f831419aacca9d480d55576485dffa7
cbrom abody <kb.cbrom@gmail.com>
2020-07-25 17:23:40 +0300

    handler not set error added

--- cmd/models/repository.go
+++ cmd/models/repository.go
@@ -1,8 +1,15 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"errors"
 
-var handler *gorm.DB
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	handlerNotSet error = errors.New("Handler not set properly")
+	handler       *gorm.DB
+)
 
 // SetRepoDB global db handler
 func SetRepoDB(db *gorm.DB) {
