9ec6fd4331dfd51d347aa002d0b8179d320a0035
cbrom abody <kb.cbrom@gmail.com>
2020-07-26 15:34:30 +0300

    restructuring of the test

--- cmd/models/user.model_test.go
+++ cmd/models/user.model_test.go
@@ -1,18 +1,12 @@
 package models_test
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/jinzhu/gorm"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"golang.org/x/crypto/bcrypt"
 
 	"go-lms-of-pupilfirst/cmd/models"
-	"go-lms-of-pupilfirst/configs"
 	"go-lms-of-pupilfirst/migrations"
-	"go-lms-of-pupilfirst/pkg/database"
 
 	"github.com/pborman/uuid"
 
@@ -21,29 +15,18 @@ import (
 
 var _ = Describe("User.Model", func() {
 	var (
-		user     models.User
-		db       *gorm.DB
-		userName string
+		user models.User
 	)
 
+	db := models.ConnectToTestDatabase()
+	migrations.Migrate(db)
+
 	BeforeEach(func() {
-
-		dbConfig, err := configs.LoadConfig()
-		if err != nil {
-			log.Printf("main : Error loading database %+v", err)
-		}
-		log.Printf("%+v", dbConfig.Storage)
-		db, err = database.Initialize(dbConfig.Storage)
-
-		models.SetRepoDB(db)
-
-		migrations.Migrate(db)
-		// defer db.Close()
 
 		password := "password"
 		passwordSalt := uuid.NewRandom().String()
 		saltedPassword := password + passwordSalt
-		passwordHash, err := bcrypt.GenerateFromPassword([]byte(saltedPassword), bcrypt.DefaultCost)
+		passwordHash, _ := bcrypt.GenerateFromPassword([]byte(saltedPassword), bcrypt.DefaultCost)
 
 		user = models.User{
 			Email:        "test1@gmail.com",
@@ -54,30 +37,57 @@ var _ = Describe("User.Model", func() {
 			About:        "About user",
 		}
 
+		if err := user.Create(); err != nil {
+			Fail("Couldn't create user")
+		}
+
 	})
 
 	AfterEach(func() {
-		db.Close()
+		// drop all users
+		u := models.User{}
+		u.Delete()
 	})
 
-	Describe("Registering a new User", func() {
-		Context("Create a new User", func() {
+	AfterSuite(func() {
+		models.CloseDB()
+	})
+
+	Describe("Basic Crud Tests", func() {
+		Context("CRUD basics", func() {
 			It("should contain the newly created user", func() {
-				if err := user.Create(); err == nil {
-					createdUser := &models.User{}
-					createdUser.SetID(user.GetID())
-					createdUser.FetchByID()
-					userName = createdUser.Name
-					Expect(userName).To(Equal(user.Name))
-				} else {
-					if err.Error() == "pq: duplicate key value violates unique constraint \"uix_users_email\"" {
-						fmt.Printf("Error %+v", err.Error())
-					} else {
-						Fail("Couldn't create user")
-					}
+				createdUser := &models.User{}
+				createdUser.SetID(user.GetID())
+				createdUser.FetchByID()
+				Expect(createdUser.Name).To(Equal(user.Name))
 
-				}
+			})
 
+			It("should fetch a user by ID", func() {
+				u := models.User{}
+				u.SetID(user.GetID())
+				u.FetchByID()
+				Expect(user.Email).To(Equal(u.Email))
+				Expect(user.PasswordHash).To(Equal(u.PasswordHash))
+			})
+
+			It("should update an existing user", func() {
+
+				user.Name = "New Name"
+				user.UpdateOne()
+				u := models.User{}
+				u.SetID(user.GetID())
+				u.FetchByID()
+				Expect(user.Name).To(Equal(u.Name))
+			})
+
+			It("should softdelete an existing user", func() {
+				user.SoftDelete()
+				u := models.User{}
+				u.SetID(user.GetID())
+				u.FetchByID()
+				Expect(u.Name).To(Equal(""))
+				Expect(u.Email).To(Equal(""))
 			})
 		})
 	})
