f586b4066afdd8976567688cac0487bd29472d62
cbrom abody <kb.cbrom@gmail.com>
2020-07-30 16:49:06 +0300

    create level model from level schema function implemented

--- cmd/graphql/schemas/level.schema.go
+++ cmd/graphql/schemas/level.schema.go
@@ -2,6 +2,8 @@ package schemas
 
 import (
 	"go-lms-of-pupilfirst/cmd/models"
+	"go-lms-of-pupilfirst/pkg/utils"
+	"time"
 
 	"github.com/graphql-go/graphql"
 )
@@ -53,4 +55,25 @@ var CreateLevelSchema = graphql.FieldConfigArgument{
 	"course_id": &graphql.ArgumentConfig{
 		Type: graphql.String,
 	},
+}
+
+// CreateLevelFromSchema adapter for level schema and level model
+func CreateLevelFromSchema(p graphql.ResolveParams) models.Level {
+	unlockOnArg := p.Args["unlock_on"]
+	var unlockOn *time.Time
+	switch unlockOnArg.(type) {
+	case string:
+		unlockOn = utils.GetTimeFromStamp(unlockOnArg.(string))
+	case time.Time:
+		unlockOn = unlockOnArg.(*time.Time)
+	}
+	level := models.Level{
+		Name:        p.Args["name"].(string),
+		CourseID:    p.Args["course_id"].(string),
+		Description: p.Args["description"].(string),
+		Number:      p.Args["number"].(int),
+		UnlockOn:    unlockOn,
+	}
+
+	return level
 }
