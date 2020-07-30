98715d6f6c95fd6e9837c570bfcdfc70726af5d5
cbrom abody <kb.cbrom@gmail.com>
2020-07-30 16:47:28 +0300

    create level function added

--- cmd/graphql/resolvers/level.resolver.go
+++ cmd/graphql/resolvers/level.resolver.go
@@ -1,10 +1,21 @@
 package resolvers
 
 import (
+	"go-lms-of-pupilfirst/cmd/graphql/schemas"
 	"go-lms-of-pupilfirst/cmd/models"
 
 	"github.com/graphql-go/graphql"
+	"github.com/pkg/errors"
 )
+
+// CreateLevel creates a new level
+func CreateLevel(p graphql.ResolveParams) (interface{}, error) {
+	level := schemas.CreateLevelFromSchema(p)
+	if err := level.Create(); err == nil {
+		return level.GetID(), nil
+	}
+	return nil, errors.New("Unable to create level")
+}
 
 // GetCourseLevels returns level by id with no graphql query
 func GetCourseLevels(p graphql.ResolveParams) (interface{}, error) {
