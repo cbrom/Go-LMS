3fb5f752c174f172a10af47481dd67cef96210de
eyasubirhanu <eyasubirhanu0@gmail.com>
2022-10-08 09:06:24 +0300

    add signup endpoint
    remove the defult pinghandeler because it's useless in this project and add signup end point for SignUp function!!!

--- cmd/api/handlers/routes.go
+++ cmd/api/handlers/routes.go
@@ -13,19 +13,14 @@ var (
 	authenticator *auth.Authenticator
 )
 
-func pingHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
-
 // ApplyRoutes applies router to gin engine
 func ApplyRoutes(r *gin.Engine, auth *auth.Authenticator, db *gorm.DB) {
 	models.SetRepoDB(db)
 	authenticator = auth
 	apiV1 := r.Group("/v1")
 	{
-		apiV1.GET("/ping", pingHandler)
+		apiV1.POST("/users/signup", SignUp)    // done
 		
+
 	}
 }
